95784c1d48361fa20f1e2af2e33746dadcfa3f58
Wisdom <wisdomokwen@gmail.com>
2024-06-30 01:12:30 -0700

    Rework experience slider

--- frontend/src/components/cards/ExperienceCard.jsx
+++ frontend/src/components/cards/ExperienceCard.jsx
@@ -1,16 +1,14 @@
 import * as React from 'react';
 import Box from '@mui/material/Box';
 import Card from '@mui/material/Card';
-import CardActions from '@mui/material/CardActions';
 import CardContent from '@mui/material/CardContent';
-import Button from '@mui/material/Button';
 import Typography from '@mui/material/Typography';
 
 
 export default function ExperienceCard(props) {
   return (
     <Box sx={{ width: 350, height: 200, margin: '0 5px', boxShadow: 3 }}>
-      <Card sx={{ marginBottom: 1, width: 350, height: 200}} variant="outlined">
+      <Card sx={{ marginBottom: 1, width: 350, height: 200 }} variant="outlined">
         <CardContent>
           <Typography  variant="h5" component="div">
             {props.company}
@@ -24,10 +22,11 @@ export default function ExperienceCard(props) {
           <Typography variant="body2">
             {props.tech_stack}
           </Typography>
+          <hr /> 
+          <Typography variant="body2">
+            {props.date}
+          </Typography>
       </CardContent>
-      {/* <CardActions>
-        <Button size="small">Learn More</Button>
-      </CardActions>       */}
       </Card>
     </Box>
   );
