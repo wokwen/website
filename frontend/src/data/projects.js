e0b89f982c6fe5da89a16369a351343242ed0b60
Wisdom Okwen <65198244+wisdom-okwen@users.noreply.github.com>
2024-06-30 22:03:22 -0400

    Populate projects (#13)
    * make footer background white
    
    * remove unnecessary components
    
    * Populate data for first 3 projects
    
    * push video

--- frontend/src/data/projects.js
+++ frontend/src/data/projects.js
@@ -7,38 +7,38 @@ const deployment_types = {
 const projects = [
     {
         id: 1,
-        title: "Contageon Simulation",
-        url: "Sample Description",
-        description: "A very good project",
-        built_with: ['Python ', 'Next ', 'React '],
-        media_type: 'video',
+        title: "Housing Price Predictor",
+        url: "https://github.com/wisdom-okwen/ml-california-housing/blob/main/california_housing.ipynb",
+        description: "An ML Model that predicts the price of a house in California based on location, size, number of rooms, age and other factors ",
+        built_with: ['ScikitLearn, ', 'Pandas, ', 'Matplotlib '],
+        deployed: false,
         deployment_type: deployment_types.youtube
     },
     {
         id: 2,
-        title: "Contageon Simulation",
-        url: "",
-        description: "A very good project",
-        built_with: ['React, '],
-        media_type: 'video',
-        deployment_type: deployment_types.undeployed
+        title: "UNC CSXL Website",
+        url: "https://final-team-c2-comp590-140-24sp-evanesce.apps.unc.edu/announcements",
+        description: "News page that helps 800+ students in the department get easy access to information about events/conferences/activities",
+        built_with: ['Angular, ', 'Python, ', 'FastAPI, ', 'SQLAlchemy'],
+        deployed: true,
+        deployment_type: deployment_types.website
     },
     {
         id: 3,
         title: "Contageon Simulation",
-        url: "",
-        description: "A very good project",
-        built_with: ['React, '],
-        media_type: 'video',
+        url: "https://github.com/wisdom-okwen/contageon-simulation",
+        description: "A simulation of how epdemics propagate within a region from first contraction until recovery",
+        built_with: ['Python'],
+        deployed: false,
         deployment_type: deployment_types.undeployed
     },
     {
         id: 4,
-        title: "Contageon Simulation",
+        title: "Ping Pong Game",
         url: "",
         description: "A very good project that can lead to great things in life asd thweojowsdf aweohwoi waoihnjweof wfo iqa wefrwfew ",
         built_with: ['React, '],
-        media_type: 'video',
+        deployed: false,
         deployment_type: deployment_types.website
     },
     {
@@ -48,6 +48,7 @@ const projects = [
         description: "A very good project",
         built_with: ['Express, ', 'React ', 'MongoDB '],
         media_type: 'video',
+        deployed: false,
         deployment_type: deployment_types.website
     },
     {
@@ -57,6 +58,7 @@ const projects = [
         description: "A very good project",
         built_with: ['React, '],
         media_type: 'video',
+        deployed: false,
         deployment_type: deployment_types.youtube
     },
     {
@@ -65,7 +67,7 @@ const projects = [
         url: "",
         description: "A very good project",
         built_with: ['React, '],
-        media_type: 'video',
+        deployed:false,
         deployment_type: deployment_types.undeployed
     }
 ]
